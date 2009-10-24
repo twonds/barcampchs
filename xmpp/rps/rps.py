7c722a31cac3ecffd3b4f1d228cc61a85ac05567
Christopher 'Tofu' Zorn <tofu@stanziq.com>
2009-10-23 21:51:06 -0400

    full game done with message protocol and slide updates

--- xmpp/rps/rps.py
+++ xmpp/rps/rps.py
@@ -1,73 +1,78 @@
 # basic rock, papers, scissors logic
 #
 
+INVALID_MOVE = -2
+WAIT_PLAYER = -1
+
+DRAW = 0
+PLAYER1 = 1
+PLAYER2 = 2
+
+PLAYER_COUNT = 2
+
 class RPS(object):
     """A simple rock, paper scissors game class.
     """
 
     def __init__(self):
-        self.players = []
-        self.moves   = []
+        self.start()
         self.VALID_MOVES = ['rock', 'paper', 'scissors']
+
+    def start(self):
+        self.moves = []
+        self.players = []
+
+    restart = start # magic rename
 
     def move(self, player, move):
         """ move - Send either rock, paper, or scissors to make your move.
         """
-        cmp = -1 # -1 means game is not over.
-        if len(self.players) == 2:
+        cmp = WAIT_PLAYER # means game is not over.
+        if len(self.players) == PLAYER_COUNT:
             if move in self.VALID_MOVES:
                 self.moves.append((player, move.lower()))
                 
-                if len(self.moves) == 2:
-                    cmp = self._cmp(self.moves[0][1], 
-                                    self.moves[1][1])
+                if len(self.moves) == PLAYER_COUNT:
+                    cmp = self._cmp(self.moves[0], 
+                                    self.moves[1])
+                    
             else:
-                cmp = -2 
+                cmp = INVALID_MOVE
         else:
             cmp = None # game hasnt started 
         return cmp
 
+    def getPlayer(self, num):
+        return self.moves[num-1]
+
 
     def _cmp(self, mv1, mv2):
-        ret_val = 0
+        ret_val = DRAW
+
         if mv1[1] == 'rock':
             if mv2[1] == 'paper':
-                ret_val = 2
+                ret_val = PLAYER2
             elif mv2[1] == 'scissors':
-                ret_val = 1
+                ret_val = PLAYER1
         elif mv1[1] == 'paper':
             if mv2[1] == 'rock':
-                ret_val = 1
+                ret_val = PLAYER1
             elif mv2[1] == 'scissors':
-                ret_val = 2
+                ret_val = PLAYER2
         elif mv1[1] == 'scissors':
             if mv2[1] == 'paper':
-                ret_val = 1
+                ret_val = PLAYER1
             elif mv2[1] == 'rock':
-                ret_val = 2
+                ret_val = PLAYER2
 
         return ret_val
         
-    def _tcmp(self, mv1, mv2):
-        ret_val = 0 # zero is a tie
-        if mv1 == 'rock' and mv2 == 'paper':
-            ret_val = 2
-        elif mv1 == 'scissors' and mv2 == 'rock':
-            ret_val = 2
-        elif mv1 == 'scissors' and mv2 == 'rock':
-            ret_val = 2
-        elif mv2 == 'rock' and mv1 == 'paper':
-            ret_val = 1
-        elif mv2 == 'scissors' and mv1 == 'rock':
-            ret_val = 1
-            
 
-        return ret_val
 
     def join(self, player):
         """ join - Join a game of rock, paper, scissors.
         """
-        if len(self.players) > 2:
+        if len(self.players) > PLAYER_COUNT:
             return False
         
         self.players.append(player)
@@ -76,12 +81,13 @@ class RPS(object):
     def help(self):
         """ help - Get help on commands.
         """
-    
         commands = str(self.__doc__)+"\nCommands : "
         for command in dir(self):
             if command[0] == '_':
                 continue
-            if  str(type(getattr(command))) == "<type 'instancemethod'>":
-                commands += "\n "+ str(getattr(self, command).__doc__)
-                
+            if  str(type(getattr(self, command))) == "<type 'instancemethod'>":
+                doc = getattr(self, command).__doc__
+                if doc is not None:
+                    commands += "\n "+ str(doc)
+                                 
         return commands
--- xmpp/rps/rpsbot.py
+++ xmpp/rps/rpsbot.py
@@ -3,11 +3,14 @@ from twisted.python import log # use this for logging
 from twisted.words.xish import domish # used for manipulating xml
 from wokkel.xmppim import MessageProtocol, AvailablePresence # protocol helpers
 
+import exceptions
+
 import rps
 
 class RPSProtocol(MessageProtocol):
     """Handle XMPP Protocol for Rock, Paper, Scissors.
     """
+
 
     def connectionMade(self):
         """This method is trigged by wokkel when 
@@ -39,26 +42,95 @@ class RPSProtocol(MessageProtocol):
         Normally, we would use xpath for different message types.
         """
         log.msg("======================== onMessage =========================")
-        
+        outcome_message = None
+
         if msg.hasAttribute('type') \
                 and msg["type"] == 'chat' \
                 and hasattr(msg, "body"):
             
-            cmd, arg_string = str(msg.body).lower().split(" ", 1)
+            cmd = str(msg.body).lower() # clean up body for handling
+            log.msg("========= trying to execute %s =============\n" % (cmd,))
             
-            method = getattr(self.rps, "_"+cmd, None)
+            player = msg['from'].lower() # get our player
+            log.msg("========= player %s =============\n" % (player,))
             
-            if method:
-                if arg_string:
-                    method(arg_string.split())
-                else:
-                    method()
+            if cmd == 'join':
+                self._doJoin(player)
+            elif cmd in self.rps.VALID_MOVES:
+                self._doGameMove(player, cmd)                
             else:
-                self.rps.help()
+                self._doHelp(player)
+
+    def _doHelp(self, player):
+        """Send help message.
+        """ 
+        outcome_message = self.rps.help()
+
+        self.sendMessage(player, outcome_message)
 
 
+    def _doJoin(self, player):
+        """Join a game to play.
+        """
+        if self.rps.join(player):
+            outcome_message = "You have joined the game!"
+        else:
+            outcome_message = "We are full."
+
+        self.sendMessage(player, outcome_message)
+
+
+    def _doGameMove(self, player, move):
+        """ Make a move.
+        """
+        game_move = self.rps.move(player, move)
+        log.msg(game_move)
+
+        outcome_message = 'Invalid move?'
+
+        if game_move is None:
+            outcome_message = 'Game has not started.'
+            if player in self.rps.players:
+                outcome_message += " Please wait."
+            else:
+                outcome_message += " Please join."
+
+        elif game_move == rps.WAIT_PLAYER:
+            outcome_message = 'Thanks, waiting for the other player.'
+        
+        elif game_move == rps.DRAW:
+            outcome_message = "You both played %s !" % (move,)
+            self.rps.restart()
+        
+        elif game_move == rps.PLAYER1:
+            outcome_message = self._wins(player, rps.PLAYER1)
+        elif game_move == rps.PLAYER2:
+            outcome_message = self._wins(player, rps.PLAYER2)
+
+        self.sendMessage(player, outcome_message)
+
+
+    def _wins(self, player, play):
+        winner = self.rps.getPlayer(play)
+
+        outcome_message = "Player %s wins with %s!" % (winner[0], winner[1],)
+
+        for p in self.rps.players:
+            if p != player and p != winner[0]:
+                self.sendMessage(p, outcome_message)
+            if p != player and p == winner[0]:
+                self.sendMessage(p, "You are the winner!")
+
+        self.rps.restart()
+
+        if winner[0] == player:
+            outcome_message = " You are the winner! "
+
+        return outcome_message
 
     def sendMessage(self, to, body):
+        """ Send an XMPP message stanza of type chat with a body.
+        """
         reply = domish.Element(('xmpp:client', "message"))
         reply["to"] = to
         reply["type"] = 'chat'
